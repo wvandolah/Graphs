e24524fd800ffe4f3cb78f832853fb42def94518
William VanDolah <william.vandolah@gmail.com>
2018-07-23 16:05:54 -0500

    added add_edge

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -17,13 +17,19 @@ class Graph:
         """
         self.vertices[vertex] = set()
 
+    def add_edge(self, start, end, bi=True):
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception("Error - vertices not in graph!")
+        self.vertices[start].add(end)
+        if bi:
+            self.vertices[end].add(start)
 
 graph = Graph()  # Instantiate your graph
 graph.add_vertex('0')
 graph.add_vertex('1')
 graph.add_vertex('2')
 graph.add_vertex('3')
-# graph.add_edge('0', '1')
-# graph.add_edge('0', '3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3', False)
 print(graph.vertices)
 
