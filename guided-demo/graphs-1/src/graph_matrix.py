0ebb0dc89d613e3f3f8aab0819055fbc50feb06b
William VanDolah <william.vandolah@gmail.com>
2018-07-23 15:34:21 -0500

    added graph class. added add_vertex method

--- guided-demo/graphs-1/src/graph_matrix.py
+++ guided-demo/graphs-1/src/graph_matrix.py
@@ -17,3 +17,31 @@ class Graph:
     def connect_vertex(self, row, col):
         """Add an edge between vertices indicated by row/col of matrix."""
         self.matrix[row][col] = 1
+
+"""
+Below if from class
+"""
+"""Live coding of different ways to represent graphs."""
+
+
+class Vertex:
+    """MVV - Minimum Viable Vertex."""
+    def __init__(self, label):
+        self.label = label
+
+    def __repr__(self):
+        return str(self.label)
+
+
+class MatrixGraph:
+    """Adjacency matrix representation of a graph."""
+    def __init__(self, num_vertices):
+        self.matrix = [[0 for _ in range(num_vertices)]
+                       for _ in range(num_vertices)]
+        self.vertices = [Vertex(str(i)) for i in range(num_vertices)]
+
+    def add_edge(self, start_index, end_index, bidirectional=True):
+        """Add an edge from start vertex to end vertex."""
+        self.matrix[start_index][end_index] = 1
+        if bidirectional:
+            self.matrix[end_index][start_index] = 1
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -8,4 +8,22 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        """We could check if a key is already in the dictionary, and send an error
+        if one is found. This would prevent being able to overwrite keys.
+        https://docs.python.org/3.7/tutorial/datastructures.html#dictionaries
+        """
+        self.vertices[vertex] = set()
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+# graph.add_edge('0', '1')
+# graph.add_edge('0', '3')
+print(graph.vertices)
+
