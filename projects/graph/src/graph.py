572068520410b87e563e35d5948ca003fdb3f2cb
William VanDolah <william.vandolah@gmail.com>
2018-07-24 16:26:38 -0500

    able to generate and draw 25 points

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -3,19 +3,24 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-
+# class Vertex:
+#     def __init__
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
 
-    def add_vertex(self, vertex):
+    def add_vertex(self, vertex, edges=()):
         """We could check if a key is already in the dictionary, and send an error
         if one is found. This would prevent being able to overwrite keys.
         https://docs.python.org/3.7/tutorial/datastructures.html#dictionaries
         """
-        self.vertices[vertex] = set()
+        if vertex in self.vertices:
+            raise Exception("Error - vertex {} is already a thing".format(vertex))            
+        if not set(edges).issubset(self.vertices):
+            raise Exception('error: connot have edge to nonexistent vertices')        
+        self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bi=True):
         if start not in self.vertices or end not in self.vertices:
@@ -25,11 +30,13 @@ class Graph:
             self.vertices[end].add(start)
 
 graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3', False)
-print(graph.vertices)
+# graph.add_vertex('0')
+# graph.add_vertex('1')
+# graph.add_vertex('2')
+# graph.add_vertex('3')
+# graph.add_edge('0', '1')
+# graph.add_edge('0', '3', False)
+
+
+
 
